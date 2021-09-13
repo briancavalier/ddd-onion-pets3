246a73aeee1c84d2ffd25aa81afface183abdfb7
Brian Cavalier <brian@briancavalier.com>
2021-09-13 13:40:12 -0400

    Unparameterize application for now

--- src/application/getPetsNear.ts
+++ src/application/getPetsNear.ts
@@ -1,22 +1,24 @@
-export type GetPets<Location, Pets> = (l: Location) => Promise<Pets>
+import { Location, Pet } from '../domain/pets'
 
-export type GetLocation<UserInfo, Location> = (u: UserInfo) => Promise<Location | null>
+export type GetPets = (l: Location) => Promise<readonly Pet[]>
+
+export type GetLocation = (u: IPAddress) => Promise<Location | null>
 
 export type IPAddress = string & { _type: 'IPAddress' }
 
-export type AdoptablePetsNear<UserInfo, Location, Pet> =
-  | { type: 'UnknownLocation', userInfo: UserInfo }
+export type AdoptablePetsNear =
+  | { type: 'UnknownLocation', ipAddress: IPAddress }
   | { type: 'Pets', location: Location, pets: readonly Pet[] }
 
-export type GetPetsNearEnv<UserInfo, Location, Pet> = {
-  getLocation: GetLocation<UserInfo, Location | null>,
-  getPets: GetPets<Location, readonly Pet[]>
+export type GetPetsNearEnv = {
+  getLocation: GetLocation
+  getPets: GetPets
 }
 
-export const getPetsNear = <UserInfo, Location, Pet>({ getLocation, getPets }: GetPetsNearEnv<UserInfo, Location, Pet>) =>
-  async (user: UserInfo): Promise<AdoptablePetsNear<UserInfo, Location, Pet>> => {
-    const location = await getLocation(user)
+export const getPetsNear = ({ getLocation, getPets }: GetPetsNearEnv) =>
+  async (ipAddress: IPAddress): Promise<AdoptablePetsNear> => {
+    const location = await getLocation(ipAddress)
     return location === null
-      ? { type: 'UnknownLocation', userInfo: user }
+      ? { type: 'UnknownLocation', ipAddress: ipAddress }
       : { type: 'Pets', location, pets: await getPets(location) }
   }
